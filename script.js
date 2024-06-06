b1fd9d26dcc9d53caf07befb145ccab57c635d89
Rafael Cabral <melloraf.cabral@gmail.com>
2024-06-06 15:48:11 -0300

    subindo projeto

---
+++ script.js
@@ -0,0 +1,9 @@
+function muda(n) {
+
+    var imgPrincipal = document.getElementById("imgPrincipal");
+    
+    imgPrincipal.src = "img/wall" + n + ".jpg";
+    imgPrincipal.style.width = "100%";
+    imgPrincipal.style.height = "100%";
+
+}
